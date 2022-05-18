bba349c22d219e3803e6f624c9546d15579b3f6d
malakggh <malekgh@ac.sce.ac.il>
2022-05-18 13:47:13 +0300

    m

--- life pulse clinic/BloodTest.cs
+++ life pulse clinic/BloodTest.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace life_pulse_clinic
 {
-    internal class BloodTest
+    public class BloodTest
     {
+        private double WBC;
+        private double Neut;
+        private double Lymph;
+        private double RBC;
+        private double HCT;
+        private double Urea;
+        private double Hb;
+        private double Crtn;
+        private double Iron;
+        private double HDL;
+        private double AP;
+        public BloodTest(double WBC,double Neut,double Lymph,double RBC,double HCT,double Urea, double Hb, double Crten, double Iron, double HDL, double AP)
+        {
+            this.WBC = WBC;
+            this.Neut = Neut;
+            this.Lymph = Lymph;
+            this.RBC = RBC;
+            this.HCT = HCT;
+            this.Urea = Urea;
+            this.Hb = Hb;
+            this.Crtn = Crten;
+            this.Iron = Iron;
+            this.HDL = HDL;
+            this.AP = AP;
+
+        }
     }
 }
--- life pulse clinic/Patient.cs
+++ life pulse clinic/Patient.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace life_pulse_clinic
 {
-    internal class Patient
+    public class Patient
     {
+        private string firstName;
+        private string lastName;
+        private string id;
+        private int age;
+        private string gender;
+        private string city;
+        private string phone;
+        private string email;
+        
+        public Patient(string firstName,string lastName,string id,int age,string gender, string city,string phone,string email)
+        {
+            this.firstName = firstName;         
+            this.lastName = lastName;
+            this.id = id;
+            this.age = age;
+            this.gender = gender;
+            this.city = city;
+            this.phone = phone;
+            this.email = email;
+        }
     }
 }
