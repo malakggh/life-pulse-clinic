e4342c3aafda99e6eb2f2ac89273357c78fc3a19
Haya177 <hayaab@ac.sce.ac.il>
2022-05-19 22:34:48 +0300

    Create Neut.cs

---
+++ life pulse clinic/Neut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace life_pulse_clinic
+{
+    internal class Neut : BloodTestX
+    {
+        private WBC wbc; 
+        public Neut(double value,WBC wbc) : base(value)
+        {
+            this.wbc = wbc;
+        }
+
+        public override string GetStatus()
+        {
+            double WBCValue = wbc.GetValue();
+            double NeutValue = GetValue();
+            if(NeutValue< WBCValue * 0.28)
+            {
+                return "Low";
+            }
+            else if (NeutValue > WBCValue * 0.54)
+            {
+                return "High";
+            }
+            else
+            {
+                return "Normal";
+            }
+        }
+    }
+}
