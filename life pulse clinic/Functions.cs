151439dc895f44ca99af21102bf5befa9cf1fe9f
malakggh <malekgh@ac.sce.ac.il>
2022-05-21 00:10:48 +0300

    add GetRecommandtion function

--- life pulse clinic/Functions.cs
+++ life pulse clinic/Functions.cs
@@ -9,6 +9,111 @@ namespace life_pulse_clinic
 {
     public class Functions
     {
+        public static string GetRecommendation(string disease)
+        {
+            if (disease == "אנמיה")
+            {
+                return "שני כדורי 10 מג של B12 ביום למשך חודש";
+            }
+            else if (disease == "דיאטה")
+            {
+                return "לתאם פגישה עם תזונאי";
+            }
+            else if (disease == "דימום")
+            {
+                return "להתפנות בדחיפות לבית החולים";
+            }
+            else if (disease == "היפרליפידמיה )שומנים בדם(")
+            {
+                return "לתאם פגישה עם תזונאי, כדור 5 מג של סימוביל ביום למשך שבוע";
+            }
+            else if (disease == "הפרעה ביצירת הדם / תאי דם")
+            {
+                string n1 = "כדור 10 מג של B12 ביום למשך חודש";
+                string n2 = "כדור 5 מג של חומצה פולית ביום למשך חודש";
+                return n1 + "\n" + n2;
+            }
+            else if (disease == "הפרעה המטולוגית")
+            {
+                return "זריקה של הורמון לעידוד ייצור תאי הדם האדומים";
+            }
+            else if (disease == "הרעלת ברזל")
+            {
+                return " להתפנות לבית החולים";
+            }
+            else if (disease == "התייבשות")
+            {
+                return "מנוחה מוחלטת בשכיבה, החזרת נוזלים בשתייה";
+            }
+            else if (disease == "זיהום")
+            {
+                return "אנטיביוטיקה ייעודית";
+            }
+            else if (disease == "חוסר בוויטמינים")
+            {
+                return " הפנייה לבדיקת דם לזיהוי הוויטמינים החסרים";
+            }
+            else if (disease == "מחלה ויראלית")
+            {
+                return "לנוח בבית";
+            }
+            else if (disease == "מחלות בדרכי המרה")
+            {
+                return " הפנייה לטיפול כירורגי";
+            }
+            else if (disease == "מחלות לב")
+            {
+                return "לתאם פגישה עם תזונאי";
+            }
+            else if (disease == "מחלת דם")
+            {
+                return "שילוב של ציקלופוספאמיד וקורטיקוסרואידים";
+            }
+            else if (disease == "מחלת כבד")
+            {
+                return "הפנייה לאבחנה ספציפית לצורך קביעת טיפול";
+            }
+            else if (disease == "מחלת כליה")
+            {
+                return "איזון את רמות הסוכר בדם";
+            }
+            else if (disease == "מחסור בברזל")
+            {
+                return "שני כדורי 10 מג של B12 ביום למשך חודש";
+            }
+            else if (disease == "מחלות שריר")
+            {
+                return "שני כדורי 5 מג של כורכום c3 של אלטמן ביום למשך חודש";
+            }else if(disease == "מעשנים")
+            {
+                return "להפסיק לעשן";
+            }else if(disease == "מחלת ריאות")
+            {
+                return "להפסיק לעשן / הפנייה לצילום רנטגן של הריאות";
+            }else if(disease == "פעילות יתר של בלוטת התריס")
+            {
+                return " Propylthiouracil להקטנת פעילות בלוטת התריס";
+            }else if(disease == "סוכרת מבוגרים")
+            {
+                return "התאמת אינסולין למטופל";
+            }else if(disease == "סרטן")
+            {
+                return " אנטרקטיניב - Entrectinib";
+            }else if(disease == "צריכה מוגברת של בשר")
+            {
+                return "לתאם פגישה עם תזונאי";
+            }else if(disease == "שימוש בתרופות שונות")
+            {
+                return "הפנייה לרופא המשפחה לצורך בדיקת התאמה בין התרופות";
+            }else if(disease == "תת תזונה")
+            {
+                return "לתאם פגישה עם תזונאי";
+            }
+            else
+            {
+                return "";
+            }
+        }
         public static int GetUsernameIndex(string username)
         {
             Excel excel = new Excel(Form1.projectPath + Form1.accountsPath, 1);
